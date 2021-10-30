915636f2baeccc9f5cd129c0a0a0599bc9587ae3
Maxime "Napolitain" Boucher <mxboucher@gmail.com>
2021-10-30 22:15:07 +0200

    menubar init

--- view/MainWindow.cpp
+++ view/MainWindow.cpp
@@ -5,8 +5,15 @@
 #include "MainWindow.hpp"
 
 MainWindow::MainWindow() {
-    auto *quit = new QAction("&Quit", this);
-    QMenu *file = menuBar()->addMenu("&File");
-    file->addAction(quit);
-    connect(quit, &QAction::triggered, qApp, QApplication::quit);
+    auto *menu_file = new QMenu("&File");
+    menu_file->addAction("&Open", qApp, SLOT(quit()), Qt::CTRL + Qt::Key_O);
+    menu_file->addAction("&Save as...", qApp, SLOT(quit()), Qt::CTRL + Qt::Key_S);
+    auto *menu_hdr = new QMenu("&HDR");
+    menu_hdr->addAction("&Merge", qApp, SLOT(quit()), Qt::CTRL + Qt::Key_M);
+
+    QMenuBar *menu_bar = menuBar();
+    menu_bar->addMenu(menu_file);
+    menu_bar->addMenu(menu_hdr);
+    menu_bar->show();
+    setMenuBar(menu_bar);
 }
