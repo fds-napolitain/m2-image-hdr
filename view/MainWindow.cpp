09b67a2436cc6097841e4c8437c947464f626503
Maxime "Napolitain" Boucher <mxboucher@gmail.com>
2021-10-30 18:04:22 +0200

    init

--- view/MainWindow.cpp
+++ view/MainWindow.cpp
@@ -5,5 +5,8 @@
 #include "MainWindow.hpp"
 
 MainWindow::MainWindow() {
-
+    auto *quit = new QAction("&Quit", this);
+    QMenu *file = menuBar()->addMenu("&File");
+    file->addAction(quit);
+    connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
