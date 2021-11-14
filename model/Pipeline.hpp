42886f83d67b58528ea151794c9bf89e3b9bb8ad
Maxime Boucher <mxboucher@gmail.com>
2021-11-14 21:58:33 +0100

    ajout pipeline (chaine d'actions de la menubar)

---
+++ model/Pipeline.hpp
@@ -0,0 +1,32 @@
+//
+// Created by Maxime Boucher on 14/11/2021.
+//
+
+#ifndef QTHDR_PIPELINE_HPP
+#define QTHDR_PIPELINE_HPP
+
+
+enum class Align {
+	NONE,
+	MTB,
+};
+
+enum class Merge {
+	NONE,
+	Debevec,
+	Mertens,
+};
+
+enum class Tonemap {
+	NONE,
+};
+
+class Pipeline {
+public:
+	Align align = Align::NONE;
+	Merge merge = Merge::NONE;
+	Tonemap tonemap = Tonemap::NONE;
+};
+
+
+#endif //QTHDR_PIPELINE_HPP
